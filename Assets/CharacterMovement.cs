3a26f528a78faeef39fee7e421c49be9e935142a
Luke Frisken <l.frisken@gmail.com>
2018-09-04 18:43:21 +1000

    initial commit with physics

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1000.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 1000.0f;
+
+		rb.AddForce(x, 0, z);
+		Debug.Log(rb.velocity);
+
+        // transform.Translate(x, 0, z);
+	}
+}
