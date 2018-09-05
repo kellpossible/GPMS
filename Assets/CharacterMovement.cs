c0291b8c948c07f225c372d6ca4c75b0ab448fe5
Luke Frisken <l.frisken@gmail.com>
2018-09-05 18:30:01 +1000

    updated character controller

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -15,9 +15,21 @@ public class CharacterMovement : MonoBehaviour {
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 1000.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 1000.0f;
 
-		rb.AddForce(x, 0, z);
-		Debug.Log(rb.velocity);
+		var jump = Input.GetButtonDown("Jump");
 
-        // transform.Translate(x, 0, z);
+		rb.AddForce(x, 0, z);
+
+		var velocity_planar = rb.velocity;
+		velocity_planar.y = 0.0f;
+
+		if (velocity_planar.magnitude > 0.1)
+		{
+			transform.rotation=Quaternion.LookRotation(velocity_planar);
+		}
+
+		if (jump) {
+			Debug.Log("Jumping");
+			rb.velocity.Set(rb.velocity.x, 4.0f, rb.velocity.z);
+		}
 	}
 }
