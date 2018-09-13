1f3d3ebe37d953ed99e4addcc41b6476f2bf6dcc
Luke Frisken <l.frisken@gmail.com>
2018-09-13 18:56:21 +1000

    Revert "trying to fix merge conflicts"
    This reverts commit 1d3339d3b9631079277172716fc83fffeed1f403, reversing
    changes made to 1d81ea46fb631c0f88f2c71cce5d877edd412a1b.

--- Assets/Scripts/ProcGen.cs
+++ Assets/Scripts/ProcGen.cs
@@ -14,7 +14,7 @@ public class MapTile
 
 public enum TileType
 {
-    Entry, Exit, Tile, Gap, Jump, Crumble, Turret, Door, Switch, Moving
+    Entry, Exit, Tile, Gap, Jump, Crumble, Door, Switch, Turret, Moving
 };
 
 public class Coords
@@ -31,46 +31,25 @@ public class Coords
 public class ProcGen : MonoBehaviour {
 
     public int tiles; //the amount of tiles to place
-    public float chance; 
-    public float baseDirChance; //chance that path changes direction
+    public float chance = 0.90f;
     public enum DIR { N, E, S, W };
-    public int posX, posY;
+    public int posX, posY = 20;
     DIR dir;
-    public int levelSize;
-    public MapTile[,] level;
-    public float tileChance = 0.5f; //chacne that a tile will spawn
-    public Coords entryCoords, exitCoords;
+    public int levelSize = 80;
+    public MapTile[,] level = new MapTile[80, 80];
+    public float tileChance = 0.5f;
+    private Coords entryCoords, exitCoords;
 
     // Use this for initialization
     void Start ()
     {
-<<<<<<< HEAD
         MapTile[,] lvl = createLevel();
         //Coords[] test = findPath(entryCoords, exitCoords);
-=======
-        level = new MapTile[levelSize, levelSize];
-        baseDirChance = chance;
-        MapTile[,] lvl = createLevel(levelSize, tiles, chance);
-
-        /*
-        //pathfinding testing 
-        Coords[][] test = createPairList(lvl);
-        printPairList(test);
-        if (findCoordInList(test, exitCoords) != null)
-        {
-            Debug.Log("coords at moveset: " + findCoordInList(test, exitCoords).x + " move: " + findCoordInList(test, exitCoords).y);
-        }
-        else
-            Debug.Log("null");
-
-        Coords[] listOfPairs = findPath(entryCoords, exitCoords, test);
-        */
->>>>>>> ProcGen
     }
 
 
-    // Update is called once per frame
-    void Update ()
+	// Update is called once per frame
+	void Update ()
     {
         
 	}
@@ -92,7 +71,7 @@ public class ProcGen : MonoBehaviour {
                 output += "_";
             }
             range += 1;
-            if(range % levelSize == 0)
+            if(range % 80 == 0)
             {
                 Debug.Log(output);
                 output = "";
@@ -104,9 +83,9 @@ public class ProcGen : MonoBehaviour {
     void findTopLeft()
     {
         //find top left tile for entry point
-        for (int x = 0; x < levelSize; x++)
+        for (int x = 0; x < 80; x++)
         {
-            for (int y = 0; y < levelSize; y++)
+            for (int y = 0; y < 80; y++)
             {
                 if (level[x, y] != null)
                 {
@@ -121,9 +100,9 @@ public class ProcGen : MonoBehaviour {
     void findBottomRight()
     { 
         //find bottom left for exit point
-        for (int x = levelSize - 1; x >= 0; x--)
+        for (int x = 79; x >= 0; x--)
         {
-            for (int y = levelSize - 1; y >= 0; y--)
+            for (int y = 79; y >= 0; y--)
             {
                 if (level[x, y] != null)
                 {
@@ -139,13 +118,13 @@ public class ProcGen : MonoBehaviour {
     { //returns a random tile, will not return moving
         TileType output = new TileType();
         float rand = Random.value * 10;
-        if (rand >= 2.0 && rand <= 6.0)
+        if (rand > 1.0 && rand <= 8.0)
         {
             output = (TileType)(int)rand;
         }
         else
         {
-            output = randomType();
+            output = TileType.Jump;
         }
         return output;
     }
@@ -154,10 +133,10 @@ public class ProcGen : MonoBehaviour {
     void populate()
     { //adds tiles to the map. Uses the tileChance float as the chance a tile is placed (can be altered to change the difficulty)
         
-        //checking for cross - this should be used for turrets
-        for (int y = 0; y < levelSize; y++)
+        //checking for cross 
+        for (int y = 0; y < 80; y++)
         {
-            for (int x = 0; x < levelSize; x++)
+            for (int x = 0; x < 80; x++)
             {
                 if (level[x, y] != null && level[x, y + 1] != null && level[x + 1, y] != null && level[x, y - 1] != null && level[x - 1, y] != null)
                 {
@@ -168,18 +147,18 @@ public class ProcGen : MonoBehaviour {
                         if(rnJesus > tileChance)
                         {
                             // level[x, y].type = TileType.Turret; //randomize this line?
-                            level[x, y].type = TileType.Turret;
+                            level[x, y].type = randomType();
                         }
                         
                     }
                 }
             }
         } 
-        /*
+
         //check for square (moving platforms)
-        for (int y = 0; y < levelSize; y++)
+        for (int y = 0; y < 80; y++)
         {
-            for (int x = 0; x < levelSize; x++)
+            for (int x = 0; x < 80; x++)
             {
                 if (level[x, y] != null && level[x, y + 1] != null && level[x + 1, y] != null && level[x + 1, y + 1] != null)
                 {
@@ -201,12 +180,12 @@ public class ProcGen : MonoBehaviour {
                     }
                 }
             }
-        } */
+        }
 
         //vertical 3 * 1 object placement
-        for (int y = 0; y < levelSize; y++)
+        for (int y = 0; y < 80; y++)
         {
-            for (int x = 0; x < levelSize; x++)
+            for (int x = 0; x < 80; x++)
             {
                 if (level[x, y] != null && level[x - 1, y] != null && level[x + 1, y] != null)
                 {
@@ -224,9 +203,9 @@ public class ProcGen : MonoBehaviour {
             }
         }
         //horizontal 3 * 1 object placement
-        for (int y = 0; y < levelSize; y++)
+        for (int y = 0; y < 80; y++)
         {
-            for (int x = 0; x < levelSize; x++)
+            for (int x = 0; x < 80; x++)
             {
                 if (level[x, y] != null && level[x, y - 1] != null && level[x, y + 1] != null)
                 {
@@ -244,9 +223,9 @@ public class ProcGen : MonoBehaviour {
             }
         }
         //2 * 1 placement vertical
-        for (int y = 0; y < levelSize; y++)
+        for (int y = 0; y < 80; y++)
         {
-            for (int x = 0; x < levelSize; x++)
+            for (int x = 0; x < 80; x++)
             {
                 if (level[x, y] != null && level[x - 1, y] != null)
                 {
@@ -264,9 +243,9 @@ public class ProcGen : MonoBehaviour {
             }
         }
         //2 * 1 placement horizontal
-        for (int y = 0; y < levelSize; y++)
+        for (int y = 0; y < 80; y++)
         {
-            for (int x = 0; x < levelSize; x++)
+            for (int x = 0; x < 80; x++)
             {
                 if (level[x, y] != null && level[x, y - 1] != null)
                 {
@@ -379,20 +358,13 @@ public class ProcGen : MonoBehaviour {
             {
                 dir = rndDir();
                 layTile(dir, TileType.Tile);
-                chance = baseDirChance;     //does this need changing to chance's initial value?
+                chance = 0.90f;
             }   
         }
     }
 
-    public MapTile[,] createLevel(int lvlSize, int numTiles, float dirChance)
+    public MapTile[,] createLevel()
     {
-        levelSize = lvlSize;
-        posX = levelSize / 2;
-        posY = levelSize / 2;
-        tiles = numTiles;
-        level = new MapTile[levelSize, levelSize];
-        chance = dirChance;
-
         //randomize initial direction and place the first tile (for debug)
         dir = rndDir();
                 
@@ -411,7 +383,7 @@ public class ProcGen : MonoBehaviour {
         if (tiles == 0)
         {
             //place tiles into the array
-            populate();
+            //populate();
             //print the array to console
             printLevel();
             tiles = -5;
@@ -494,7 +466,7 @@ public class ProcGen : MonoBehaviour {
 
     public bool isExit(Coords pos)
     {
-        if (pos.x == exitCoords.x && pos.y == exitCoords.y)
+        if (pos == exitCoords)
         {
             return true;
         }
@@ -502,140 +474,38 @@ public class ProcGen : MonoBehaviour {
             return false;
     }
 
-    public bool isSame(Coords i, Coords j)
+    /*
+    public Coords[][] findPath(Coords start, Coords finish)
     {
-        if (i.x == j.x && i.y == j.y)
+        Coords[,][] temp = new Coords[100,100][];
+        temp[0,0] = checkMoves(start, (int)DIR.N);
+
+        Debug.Log("moves from start");
+        int i = 1 ,j = 1;
+        foreach (Coords pos in temp[0,0])
         {
-            return true;
+            temp[i,j] = checkMoves(pos, )
         }
-        else return false;
+
+
+
+        return null;
     }
+
+    */
+
 
     public Coords[][] createPairList(MapTile[,] level)
     {
         Coords[][] pairs = new Coords[60][];
-        int i = 0;
         foreach(MapTile tile in level)
         {
-            if (tile != null)
-            {
-                pairs[i] = checkMoves(tile.pos); //stores the list of possible moves for each tile
-                i++;
-            }
-        }
-        return pairs;
-    }
 
-    void printPairList(Coords[][] list)
-    {
-        string output = "";
-        int row = 0;
-        foreach(Coords[] moves in list)
-        {
-            if (moves != null)
-            {
-                output += row + ": ";
-                foreach (Coords move in moves)
-                {
-                    if (move != null)
-                    {
-                        output += "(" + move.x + ", " + move.y + ") ";
-                    }
-                }   
-            }
-            Debug.Log(output);
-            output = "";
-            row++;
         }
-    }
+        
 
-    public Coords findCoordInList(Coords[][] pairList, Coords find)
-    {
-        //finds a coordinate within the full pairs list
-        //returns two ints as coords
-        int moveset, move;
-        for(moveset = 0; moveset < 60; moveset++)
-        {
-            if(pairList[moveset] != null)
-            {
-                move = 0;
-                if (pairList[moveset][0] != null)
-                {
-                    if(pairList[moveset][0].x ==find.x && pairList[moveset][0].y == find.y)
-                    {
-                        Coords output = new Coords(moveset, move);
-                        return output;
-                    }
-                }
-                move += 1;
-            }
-        }
         return null;
     }
 
-    public Coords getMove(Coords[][] pairList, Coords input, int j)
-    { //takes in list, starting coordinates
-        Coords output;
-        if (pairList[(findCoordInList(pairList, input).x)][j] != null) //available moves > 0?
-        {
-            output = pairList[(findCoordInList(pairList, input).x)][j];
-            return output;
-        }
-        else
-        {
-            return null;
-        }
-    }
-    
-    public Coords[] findPath(Coords start, Coords finish, Coords[][] pairList)
-    {//takes in start and end coords, and the list of pairs for the level
-        //setting up variables
-        int i = 0, j = 1;
-        Coords[] output = new Coords[60];
-        int[] jVal = new int[60];
-        for(int x = 0; x < 60; x++)
-        {
-            jVal[x] = 1;
-        }
-        output[i] = start;
-        i++;
-        output[i] = start;
-        do
-        {
-            if (getMove(pairList, output[i], jVal[i]) != null)
-            {
-                if (!isSame(output[i], getMove(pairList, output[i - 1], jVal[i])))
-                {
-                    Debug.Log("move found");
-                    output[i + 1] = getMove(pairList, output[i], jVal[i]);
-                    i++;
-                    
-                }
-                else
-                {
-                    if (getMove(pairList, output[i - 1], jVal[i - 1] + 1) != null && i > 0)
-                    {
-                        if (isSame(getMove(pairList, output[i - 1], jVal[i - 1] + 1), getMove(pairList, output[i], jVal[i])))
-                        {
-                            Debug.Log("duplicate move found, trying j + 2");
-                            output[i] = getMove(pairList, output[i - 1], jVal[i - 1] + 2);
-                            jVal[i - 1]++;
-                        }
-                        else
-                        {
-                            Debug.Log("duplicate move found, trying j + 1");
-                            output[i] = getMove(pairList, output[i - 1], jVal[i - 1] + 1);
-                            jVal[i - 1]++;
-                        }
-                    }
-                }
-            }
-        Debug.Log(i + "th coord is (" + output[i].x + ", " + output[i].y + ")");
-        } while (!isExit(output[i]));   
-        return output;
-        
-    }
-
-    
 
 }
