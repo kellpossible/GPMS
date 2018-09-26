22022043abff30e6f962e215fc3c9d1ae5b7ba04
Dale de Silva <daledesilva@gmail.com>
2018-09-26 18:40:44 +1000

    Entry, Exit, and Turret tiles visible, and new GameCtrl Demo code and transition demo

--- Assets/Scripts/TileBase.cs
+++ Assets/Scripts/TileBase.cs
@@ -6,14 +6,49 @@ public class TileBase : MonoBehaviour {
 
 	public int[] ArrayIndices = new int[2];
 	public TileType TileType;
+	private Animator tileAnimator;
+	private AnimationClip[] animationClips;
 
 	// Use this for initialization
 	void Start () {
-		
+		tileAnimator = gameObject.GetComponent<Animator>();
+		animationClips = tileAnimator.runtimeAnimatorController.animationClips;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+
+	public void TransitionOn() {
+		gameObject.GetComponent<Animator>().Play("on_popUp");
+        gameObject.SetActive(true);
+	}
+
+
+	public void TransitionOff() {
+
+		tileAnimator.Play("off_floatUp");
+		float lengthOfAnim = getStateLength("off_floatUp");
+
+		Destroy(gameObject, lengthOfAnim);
+	}
+
+
+
+	private float getStateLength(string clipName)
+	{
+		float clipLength = 0;
+		foreach(AnimationClip clip in animationClips)
+        {
+            if( clip.name==clipName ) {
+				clipLength = clip.length;
+				break;
+			}
+        }
+
+		return clipLength;
+
 	}
 }
