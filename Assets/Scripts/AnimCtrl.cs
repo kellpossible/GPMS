3560856ded1d1bb1c2c2dcf04e8cf6034f9dd333
w0nght <wwongheitung@gmail.com>
2018-10-17 18:01:15 +1100

    animations and animator commuicating..

--- Assets/Scripts/AnimCtrl.cs
+++ Assets/Scripts/AnimCtrl.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class AnimCtrl : MonoBehaviour {
 
-    public GameObject Player;
-    public GameObject Button;
     public Animator anim;
+    public bool doorUnlock;
+    public bool exitEnter;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        anim = GetComponent<Animator>();
+        doorUnlock = false;
+        exitEnter = false;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        //if (CompareTag(Player))
-		
-	}
+        // FOR TEST ONLY
+        /*
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            anim.SetTrigger("Active");
+        }
+        */
 
-    void OnTriggerEnter (Collider col)
-    {
+
+    }
+
+    void OnTriggerEnter (Collider col) {
         if (col.tag == "Player" ) {
-          // TODO 
-          // button's animation.play
+            anim.SetTrigger("Active");
+
+            Debug.Log("player actived the activator");
+            doorUnlock = true;
+            anim.SetTrigger("Unlock");
+            Debug.Log("exit is now unlocked");
+
+
+        }
+    }
+
+    void CheckState ()
+    {
+        //if (doorUnlock && exitEnter)
+        if (doorUnlock)
+        {
+            Debug.Log("player has reached the exit");
+            Debug.Log("Level Completed.");
+
         }
     }
 }
