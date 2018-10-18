8a9e854142ae14a6003ced5090e38f6b22e75d50
w0nght <wwongheitung@gmail.com>
2018-10-18 19:21:15 +1100

    new changes of animation stffs

--- Assets/Scripts/AnimCtrl.cs
+++ Assets/Scripts/AnimCtrl.cs
@@ -4,52 +4,48 @@ using UnityEngine;
 
 public class AnimCtrl : MonoBehaviour {
 
-    public Animator anim;
+    private Animator anim;
     public bool doorUnlock;
-    public bool exitEnter;
+    public bool exitEntered;
+    public bool exitEnterAvailable;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
         anim = GetComponent<Animator>();
         doorUnlock = false;
-        exitEnter = false;
-
+        exitEntered = false;
+        exitEnterAvailable = false;
     }
 	
 	// Update is called once per frame
-	void Update () {
-        // FOR TEST ONLY
-        /*
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            anim.SetTrigger("Active");
-        }
-        */
-
-
-    }
-
-    void OnTriggerEnter (Collider col) {
-        if (col.tag == "Player" ) {
-            anim.SetTrigger("Active");
-
-            Debug.Log("player actived the activator");
-            doorUnlock = true;
-            anim.SetTrigger("Unlock");
-            Debug.Log("exit is now unlocked");
-
-
-        }
-    }
-
-    void CheckState ()
+	void Update ()
     {
-        //if (doorUnlock && exitEnter)
-        if (doorUnlock)
-        {
-            Debug.Log("player has reached the exit");
-            Debug.Log("Level Completed.");
+           if (Input.GetKeyDown(KeyCode.P))
+            {
+                anim.SetTrigger("Unlock");
+                exitEnterAvailable = true;
+            }
+            
+    }
 
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Player" ) {
+            doorUnlock = true;
+            anim.SetTrigger("Active");
+            //anim.SetTrigger("Unlock");
+          //  anim.SetTrigger("Touch");
+            //anim.SetBool("Unlock", true);
+
+            Debug.Log("door unlocked.");
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            anim.SetTrigger("Unlock");
         }
     }
 }
